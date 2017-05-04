a376479223824ca291246b7c7d04d5031e82686d
ilking <king.ian.ilk@gmail.com>
2017-05-04 10:45:33 -0400

    Implmented Strategy Pattern
    Added concrete brain classes.

---
+++ DPTowerBrain.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TowerHoppingProblem
+{
+  /** Given a list of integers representing towers of various heights, find out if its possible to escape the towers by jumping from one tower to another. 
+     * You can only jump as far as the height of the current tower.
+     */
+  public class DPTowerBrain : TowerBrain
+  {
+    private bool[] _dpFinds;
+    private bool _dpResult;
+    
+    public DPTowerBrain(int[] towers) : base(towers)
+    {
+      _dpResult = false;
+      _dpFinds = new bool[towers.Length];
+    }
+    
+    public override void Calculate() 
+    {
+      int i = Towers.Length - 1;
+      
+      while (i >= 0)
+      {
+        if (i + Towers[i] >= Towers.Length)
+        {
+          _dpFinds[i] = true;
+        }
+        else
+        {
+          _dpFinds[i] = false;
+          for (int j = i+1; j <= i + Towers[i]; j++)
+          {
+            if (j >= Towers.Length || _dpFinds[j] == true)
+            {
+              _dpFinds[i] = true;
+              break;
+            }
+          }
+        }
+        i--;
+      }
+      
+      _dpResult = _dpFinds[0];
+    }
+    
+    public override string ToString()
+    {
+      StringBuilder sb = new StringBuilder();
+      sb.AppendLine("Dynamic Programming Solution:");
+      sb.AppendLine(String.Format("\tTowers: {0}", base.ToString()));
+      sb.AppendLine(String.Format("\tResults: {0}", this._dpResult));
+      sb.AppendLine(String.Format("\tFindings: [{0}]", String.Join(", ", this._dpFinds)));
+      
+      return sb.ToString();
+    }
+    
+  }
+}
---
+++ SimpleTowerBrain.cs
@@ -0,0 +1,85 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TowerHoppingProblem
+{
+  /** Given a list of integers representing towers of various heights, find out if its possible to escape the towers by jumping from one tower to another. 
+     * You can only jump as far as the height of the current tower.
+     */
+  public class SimpleTowerBrain : TowerBrain
+  {
+    private readonly List<int> _steps;
+    private bool _simpleResult;
+    
+    public SimpleTowerBrain(int[] towers) : base(towers)
+    {
+      _steps = new List<int>();
+      _steps.Add(0);
+      _simpleResult = false;
+    }
+    
+    public override void Calculate() 
+    {
+    
+      int current = 0;
+      
+      while (true)
+      {
+        if (Towers[current] == 0) 
+        {
+          _simpleResult = false;
+          break;
+        }
+        else
+        {
+          int next_step = _NextStep(current);
+          if (next_step >= Towers.Length) 
+          {
+            _steps.Add(next_step);
+            _simpleResult = true;
+            break;
+          }
+          else {
+            _steps.Add(next_step);
+            current = next_step;
+          }
+        }
+      }
+    }
+    
+    private int _NextStep(int current)
+    {
+      int max_i = current;
+      int max_tower_value = current + Towers[current];
+      
+      int curr_i = current + Towers[current];
+      
+      while (curr_i > current)
+      {
+        if (curr_i >= Towers.Length) return curr_i;
+        
+        if (curr_i + Towers[curr_i] >= max_tower_value)
+        {
+          max_i  = curr_i;
+          max_tower_value = curr_i + Towers[curr_i];
+        }
+        
+        curr_i--;
+      }
+      
+      return max_i;
+    }
+    
+    public override string ToString() {
+      StringBuilder sb = new StringBuilder();
+      sb.AppendLine("Simple Solution:");
+      sb.AppendLine(String.Format("\tTowers: {0}", base.ToString()));
+      sb.AppendLine(String.Format("\tResult: {0}", this._simpleResult));
+      sb.AppendLine(String.Format("\tPath: {0}", String.Join(" -> ", this._steps)));
+      
+      return sb.ToString();
+    }
+    
+  }
+}
